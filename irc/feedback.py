2ef226141eba02048b4583b82d3e4793aa1b737f
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2016-05-15 18:47:11 +0200

    [#3] PR/RC curve. closes #3

--- irc/feedback.py
+++ irc/feedback.py
@@ -1,17 +1,17 @@
-from irc import constants
 from irc import config
-from irc import utils
+from irc import constants
 from irc import evaluation
+from irc import utils
+from irc import domain
 
 
 def usage():
-
     msg = """
-    Usage:  python2.7 -m irc.feedback --index [index] --q [ID]
+    Usage:  python -m irc.feedback --index [index] --q [ID]
 
     Where:
 
-        --index             : Index type. Options: Binary, TF, TF-IDF, TF-IDF-S
+        --index             : Index type. Options: Binary, TF, TF-IDF, TF-IDF-Prob
         --q                 : Query: 1-30
         --n                 : Number of matches to be returned. Default is 10, * for all
     """
@@ -20,7 +20,6 @@ def usage():
 
 
 def parse_args(inp):
-
     args = {}
     argc = len(inp)
 
@@ -30,10 +29,10 @@ def parse_args(inp):
             if i + 1 >= argc:
                 raise RuntimeError('Missing value for parameter --index')
 
-            if inp[i+1] not in constants.INDEX_MODELS:
-                raise RuntimeError('`{}` is not a valid Index model'.format(inp[i+1]))
+            if inp[i + 1] not in constants.INDEX_MODELS:
+                raise RuntimeError('`{}` is not a valid Index model'.format(inp[i + 1]))
 
-            args['index'] = inp[i+1]
+            args['index'] = inp[i + 1]
 
         elif inp[i] in ('--q', '-q'):
             if i + 1 >= argc:
@@ -43,7 +42,7 @@ def parse_args(inp):
                 args['q'] = int(inp[i + 1])
             except ValueError:
                 raise RuntimeError(
-                    'Invalid --q value: {}. Should be integer between 1 and 30, inclusive'.format(inp[i+1])
+                    'Invalid --q value: {}. Should be integer between 1 and 30, inclusive'.format(inp[i + 1])
                 )
 
     required_params = ('index', 'q')
@@ -53,7 +52,7 @@ def parse_args(inp):
             raise RuntimeError('Missing parameter {}'.format(param))
 
     if 'n' not in args:
-        args['n'] = 10
+        args['n'] = 15
 
     return args
 
@@ -64,28 +63,103 @@ def pprint(index, ranking):
         print("[Rank = {:3}, Score = {:.3f}] {}".format(i + 1, rank[1], index.corpus.docs[rank[0]]))
 
 
+def evaluate_query(query, ranking, relevance):
+
+    assert isinstance(query, domain.Query)
+
+    query.num_docs_retrieved = len(ranking)
+    query.relevant_docs = relevance[query.id]
+
+    num_relevant_docs_retrieved = 0
+    std_precisions, std_recalls = [], [x / float(10) for x in range(0, 10 + 1, 1)]
+    precisions, recalls = [], []
+
+    for i, rank in enumerate(ranking):
+
+        _doc_id, _doc_score = rank
+
+        if _doc_id in query.relevant_docs:
+            num_relevant_docs_retrieved += 1
+
+        _precision = float(num_relevant_docs_retrieved) / (i + 1)
+        _recall = float(num_relevant_docs_retrieved) / len(query.relevant_docs)
+        precisions.append(_precision)
+        recalls.append(_recall)
+
+    query.num_relevant_docs_retrieved = num_relevant_docs_retrieved
+    query.precision = float(num_relevant_docs_retrieved) / len(ranking)
+    query.recall = float(num_relevant_docs_retrieved) / len(query.relevant_docs)
+
+    for i, std_recall in enumerate(std_recalls):
+        std_precision = max([0] + [x for j, x in enumerate(precisions) if recalls[j] >= std_recall])
+        std_precisions.append(std_precision)
+
+    return std_precisions, std_recalls
+
+
+def mkfigure(scores):
+    import os.path
+
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+
+    plt.clf()
+
+    for _v in scores:
+        plt.plot(_v[1][1], _v[1][0], '--', linewidth=2, label=str(_v[0]))
+
+    plt.title('Feedback System Evaluation')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision at recall level x')
+    plt.legend()
+
+    figname = os.path.join(config.PROJECT_BASE, 'img/feedback.png')
+
+    if os.path.exists(figname):
+        os.remove(figname)
+
+    if not os.path.exists(os.path.dirname(figname)):
+        os.makedirs(os.path.dirname(figname))
+
+    plt.savefig(figname)
+
+    plt.close()
+
+    print('Updated feedback curve @{}'.format(figname))
+
+
 if __name__ == '__main__':
 
     import sys
 
     try:
         args = parse_args(sys.argv[1:])
+        scores = []
+        turn = 0
 
         print('Loading corpus...')
 
         corpus = utils.read_corpus_from_file(config.FILES['corpus'])
         queries = {query.id: query for query in utils.read_queries(config.FILES['queries'])}
+        relevance = utils.read_relevance(config.FILES['relevance'])
 
         index = evaluation.model(args['index'])(corpus)
         q = queries[args['q']]
-        results = index.search(q=q.term)
+        results = index.search(q=q.term, n=args['n'])
+
+        scores.append((turn, evaluate_query(q, results, relevance)))
+        turn += 1
 
         pprint(index, ranking=results)
+        print('Precision({:.3f}), Recall({:.3f})'.format(q.precision, q.recall))
+
+        mkfigure(scores)
 
         while True:
 
             try:
-                inp = input('Choose top N results to give feedback: ')
+                inp = input('\nChoose top N results to give feedback: ')
                 n = int(inp)
             except ValueError:
                 print('[ERROR] N should an integer between 1 and {}. Try again...'.format(len(results)))
@@ -97,7 +171,7 @@ if __name__ == '__main__':
 
                 while True:
                     try:
-                        inp = input('List the IDs of the relevant docs (e.g. 1, 13, 46): ')
+                        inp = input('\nList the IDs of the relevant docs (e.g. 1, 13, 46): ')
                         relevant = [int(x) for x in inp]
                     except ValueError:
                         print('[ERROR]: The IDs should an integers. Try again...')
@@ -106,16 +180,21 @@ if __name__ == '__main__':
 
                         feedback = set(focused) & set(relevant)
                         print(
-                                    '[WARN] Ignoring docs not present in top {}: {}'.format(
+                            '[WARN] Ignoring docs not present in top {}: {}'.format(
                                 n, [x for x in feedback if x not in relevant]
-                             )
+                            )
                         )
 
                         results = index.search(q=q.term, feedback=feedback)
                         pprint(index, ranking=results)
+
+                        scores.append((turn, evaluate_query(q, results, relevance)))
+                        print('Precision({:.3f}), Recall({:.3f})'.format(q.precision, q.recall))
+                        mkfigure(scores)
+                        turn += 1
                         break
 
-                # TODO: precision & recall
+                        # TODO: precision & recall
 
     except RuntimeError as err:
         print(err)
